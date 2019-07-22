75b41fafbc6f05806731a28d9d7de7d465997759
Decosan <junx0401@gmail.com>
2019-07-22 13:29:27 +0900

    DB作成、userモデル、コントローラー作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,51 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all.page(params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] ='Signed up!!'
+      redirect_to users_path
+    else
+      flash[:danger] ='Failed..'
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(prams[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] ='Edit Completed!!'
+      redirect_to user_path(@user)
+    else
+      flash[:danger] ='Failed..'
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to new_user_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  end
+end
